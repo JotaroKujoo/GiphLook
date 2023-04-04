d493766fc46a2797ee44d16689480ed5d193b613
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-04-04 14:55:14 +0200

    add: añadiendo buscador

--- giphlooker/src/pages/Home/index.js
+++ giphlooker/src/pages/Home/index.js
@@ -1,23 +1,50 @@
-import React from "react"
-import { Link } from "wouter"
+import React, { useState } from "react"
+import { Link,useLocation } from "wouter"
 import "./index.css"
 
 const POPULAR_GIFS = ["Goku", "Jotaro", "Sonic", "Mario"]
 
-export default function Home () {
+export default function Home() {
+    const [keyword, setKeyword] = useState("")
+    const [path, pushLocation] = useLocation()
+    
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        pushLocation(`/search/${keyword}`)
+    }
+
+    const handleChange = evt => {
+        setKeyword(evt.target.value)
+    }
+
+
+
     return (
         <>
             <h3>Los gifs mas populares</h3>
 
-            {
-                POPULAR_GIFS.map((popularGif) => (
-                    <li key={popularGif}>
-                        <Link className="linkDesign" to={`/search/${popularGif}`}>
-                            Gifs de {popularGif}
-                        </Link>
-                    </li>
-                ))
-            }
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Search for a gif..."
+                    onChange={handleChange}
+                    value={keyword}
+                />
+                <button>Buscar</button>
+
+            </form>
+
+            <div className="listContainer">
+                {
+                    POPULAR_GIFS.map((popularGif) => (
+                        <li key={popularGif}>
+                            <Link className="linkDesign" to={`/search/${popularGif}`}>
+                                Gifs de {popularGif}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </div>
         </>
     )
 }
